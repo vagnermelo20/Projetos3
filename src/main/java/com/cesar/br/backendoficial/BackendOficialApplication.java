f8777a6e7eb01635709a61398601fc620ee0a02a
Vagner Montenegro de Melo <vagnermontenegro2@gmail.com>
2025-10-13 17:56:15 -0300

    Parte do Backend do Projeto criado

---
+++ src/main/java/com/cesar/br/backendoficial/BackendOficialApplication.java
@@ -0,0 +1,13 @@
+package com.cesar.br.backendoficial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendOficialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendOficialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cesar/br/backendoficial/BackendOficialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesar.br.backendoficial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendOficialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
